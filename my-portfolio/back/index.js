0933ca822ce28fe4215db10d4444994110de22de
Eduardo Andres Abelardo <88063168+eduabelardo@users.noreply.github.com>
2022-10-12 18:03:21 -0300

    .

--- my-portfolio/back/index.js
+++ my-portfolio/back/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
-app.listen(5000, () => console.log('Server Running'));
+app.listen(process.env.PORT || 5000, () => console.log('Server Running'));
 
 const contactEmail = nodemailer.createTransport({
 	service: 'gmail',
